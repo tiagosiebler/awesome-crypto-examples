d43a5a7711436d3a7767f54e33c7e738ea1d6bef
Jerko J <83344666+JJ-Cro@users.noreply.github.com>
2024-08-30 19:45:31 +0200

    feat(): added small binance installation example

---
+++ src/installation/binance.ts
@@ -0,0 +1,13 @@
+
+// Import main client at the top
+import { MainClient } from 'binance';
+
+// initialise the client
+const spotClient = new MainClient({
+  api_key: 'insert api key here',
+  api_secret: 'insert api secret here',
+});
+
+
+
+
